4618a61356351b11ed872dc592846319bf303e39
nobodyme <nobodydisturbme@gmail.com>
2019-05-29 00:24:08 +0530

    added new detail songs api

--- backend/songController.js
+++ backend/songController.js
@@ -1,29 +1,44 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var songModel = require("./songSchema");
+var songModel = require('./songSchema');
 
-router.get("/list", (req, res) => {
+router.get('/list', (req, res) => {
 	songModel
 		.find({ rank: { $lte: 50 } })
 		.then(songs => {
 			res.json({ songs: songs });
 		})
 		.catch(err => {
-			res.json({ err: err });
+			res.json({ err: err.message });
 		});
 });
 
-router.get("/search", (req, res) => {
+router.get('/detail', (req, res) => {
+	const id = req.query.id;
+	if (!id) {
+		return res.json({ error: 'Data insufficient' });
+	}
+	songModel
+		.find({ _id: id })
+		.then(song => {
+			res.json({ song: song });
+		})
+		.catch(err => {
+			res.json({ err: err.message });
+		});
+});
+
+router.get('/search', (req, res) => {
 	var query = req.query.query;
 	// if query is empty send all data
 	songModel
 		.find({
 			$or: [
 				{
-					name: { $regex: query, $options: "i" }
+					name: { $regex: query, $options: 'i' }
 				},
 				{
-					artists: { $regex: query, $options: "i" }
+					artists: { $regex: query, $options: 'i' }
 				}
 			]
 		})
@@ -31,7 +46,7 @@ router.get("/search", (req, res) => {
 			return res.json({ songs: songs });
 		})
 		.catch(err => {
-			return res.json({ message: "Cannot query db" });
+			return res.json({ message: 'Cannot query db' });
 		});
 });
 
--- frontend/src/components/songDetail.js
+++ frontend/src/components/songDetail.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
+import axios from '../config';
 import '../styles/components/songDetail.css';
 
 class SongDetail extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			song: []
+		};
+	}
+	componentDidMount() {
+		axios
+			.get('/songs/list')
+			.then(response => {
+				this.setState({ song: response.data.songs });
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}
 	render() {
 		return <div>Details of the song here</div>;
 	}
