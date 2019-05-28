7a20761947de55aeb6b1e68efe52c199eb9299a3
nobodyme <nobodydisturbme@gmail.com>
2019-05-29 02:20:11 +0530

    basic router added for container

--- frontend/src/container/listContainer.js
+++ frontend/src/container/listContainer.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Header from '../components/header';
 import SongList from '../components/songList';
+import SongDetail from '../components/songDetail';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class ListContainer extends Component {
 	render() {
 		return (
-			<div>
-				<Header />
-				<SongList />
-			</div>
+			<BrowserRouter>
+				<Route path="/" component={Header} />
+				<Route path="/" exact component={SongList} />
+				<Route path="/detail/" component={SongDetail} />
+			</BrowserRouter>
 		);
 	}
 }
