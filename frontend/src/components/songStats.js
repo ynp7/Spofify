2f6290d0741aa851f734307944d26d71c4449b50
nobodyme <nobodydisturbme@gmail.com>
2019-06-02 01:14:21 +0530

    changed to dynamic layout

--- frontend/src/components/songStats.js
+++ frontend/src/components/songStats.js
@@ -2,122 +2,35 @@ import React from 'react';
 import '../styles/components/songStats.css';
 
 function SongStats({ song }) {
-	const total = [1000, 1, 10, 10, 10, 1, 1, 1, 1, 1, 0.005];
+	const total = [0, 0, 0, 1, 1, 11, 20, 10, 1, 1, 1, 1, 1, 200, 0, 10, 0];
+	let i = -1;
+
 	return (
 		<div className="songStats">
-			<div className="statItem">
-				<div className="statItemHeader">
-					<div className="statTitle">Tempo</div>
-					<div className="statValue">{song.tempo}</div>
-				</div>
-				<div className="statBar">
-					<div className="defaultStatBar" />
-					<div
-						className="valueStatBar"
-						style={{ width: `${(song.tempo * 100) / total[0]}px` }}
-					/>
-				</div>
-			</div>
-			<div className="statItem">
-				<div className="statItemHeader">
-					<div>Energy</div>
-					<div>{song.energy}</div>
-				</div>
-				<div className="statBar">
-					<div className="defaultStatBar" />
-					<div className="valueStatBar" />
-				</div>
-			</div>
-			<div className="statItem">
-				<div className="statItemHeader">
-					<div>Key</div>
-					<div>{song.key}</div>
-				</div>
-				<div className="statBar">
-					<div className="defaultStatBar" />
-					<div className="valueStatBar" />
-				</div>
-			</div>
-			<div className="statItem">
-				<div className="statItemHeader">
-					<div>Loudness</div>
-					<div>{song.loudness}</div>
-				</div>
-				<div className="statBar">
-					<div className="defaultStatBar" />
-					<div className="valueStatBar" />
-				</div>
-			</div>
-			<div className="statItem">
-				<div className="statItemHeader">
-					<div>Mode</div>
-					<div>{song.mode}</div>
-				</div>
-				<div className="statBar">
-					<div className="defaultStatBar" />
-					<div className="valueStatBar" />
-				</div>
-			</div>
-			<div className="statItem">
-				<div className="statItemHeader">
-					<div>Speechiness</div>
-					<div>{song.speechiness}</div>
-				</div>
-				<div className="statBar">
-					<div className="defaultStatBar" />
-					<div className="valueStatBar" />
-				</div>
-			</div>
-			<div className="statItem">
-				<div className="statItemHeader">
-					<div>Acousticness </div>
-					<div>{song.acousticness}</div>
-				</div>
-				<div className="statBar">
-					<div className="defaultStatBar" />
-					<div className="valueStatBar" />
-				</div>
-			</div>
-			<div className="statItem">
-				<div className="statItemHeader">
-					<div>Valence</div>
-					<div>{song.valence}</div>
-				</div>
-				<div className="statBar">
-					<div className="defaultStatBar" />
-					<div className="valueStatBar" />
-				</div>
-			</div>
-			<div className="statItem">
-				<div className="statItemHeader">
-					<div>Danceability</div>
-					<div>{song.danceability}</div>
-				</div>
-				<div className="statBar">
-					<div className="defaultStatBar" />
-					<div className="valueStatBar" />
-				</div>
-			</div>
-			<div className="statItem">
-				<div className="statItemHeader">
-					<div>Liveness</div>
-					<div>{song.liveness}</div>
-				</div>
-				<div className="statBar">
-					<div className="defaultStatBar" />
-					<div className="valueStatBar" />
-				</div>
-			</div>
-			<div className="statItem">
-				<div className="statItemHeader">
-					<div>Instrumentalness</div>
-					<div>{song.instrumentalness}</div>
-				</div>
-				<div className="statBar">
-					<div className="defaultStatBar" />
-					<div className="valueStatBar" />
-				</div>
-			</div>
+			{Object.keys(song).map(key => {
+				i++;
+				if (i > 2 && i !== 14 && i !== 16) {
+					return (
+						<div className="statItem">
+							<div className="statItemHeader">
+								<div className="statTitle">{key.split('_').join(' ')}</div>
+								<div className="statValue">{song[key]}</div>
+							</div>
+							<div className="statBar">
+								<div className="defaultStatBar" />
+								<div
+									className="valueStatBar"
+									style={{
+										width: `${(Math.abs(song[key]) * 100) / total[i]}%`
+									}}
+								/>
+							</div>
+						</div>
+					);
+				} else {
+					return <></>;
+				}
+			})}
 		</div>
 	);
 }
