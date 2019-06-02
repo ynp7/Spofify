d051e4e90876cce6ebb36c6a41417ed2956f5f70
nobodyme <nobodydisturbme@gmail.com>
2019-06-02 17:13:37 +0530

    added transition effect to stat bar

--- frontend/src/components/statBar.js
+++ frontend/src/components/statBar.js
@@ -2,15 +2,17 @@ import React from 'react';
 import '../styles/components/statBar.css';
 
 function StatBar({ song, songAttr, maxValue }) {
+	let valueStatBarRef = React.createRef();
+
+	setTimeout(() => {
+		valueStatBarRef.current.style.width = `${(Math.abs(song[songAttr]) * 100) /
+			maxValue}%`;
+	}, 100);
+
 	return (
 		<div className="statBar">
 			<div className="defaultStatBar" />
-			<div
-				className="valueStatBar"
-				style={{
-					width: `${(Math.abs(song[songAttr]) * 100) / maxValue}%`
-				}}
-			/>
+			<div className="valueStatBar" ref={valueStatBarRef} />
 		</div>
 	);
 }
